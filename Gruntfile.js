e9b04665d337bc4e69ca0b6358b3ec6755742c26
seema shariat <seemashariat@gmail.com>
2016-06-17 11:49:27 -0400

    configures concat task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,16 @@
 'use strict';
-module.exports = function (grunt) {
-    // Do grunt-related things in here
+module.exports = function (grunt) { //wrapper func
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: '\n*****\o/*****' // one of concat's built-in options
+            },
+            build: {
+                src: 'src/*.txt',
+                dest: 'build/big.txt'
+            }
+        }
+    });
 };
