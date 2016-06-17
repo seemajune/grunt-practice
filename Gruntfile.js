850562309cfd292413db3d3a91e7a6372757b796
seema shariat <seemashariat@gmail.com>
2016-06-17 13:29:20 -0400

    added watch to conduct concat task for all events within the src dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,19 @@ module.exports = function (grunt) { //wrapper func
                 src: 'src/*.txt',
                 dest: 'build/big.txt'
             }
+        },
+        watch: {
+            concat: {
+                files: ['src/*.txt'],
+                tasks: ['concat'],
+                options: {
+                    event: ['all'] //set options for added/deleted/changed or all to log to console by default the action taken
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat'); // load the plugin that provides the concat task
-    
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
 };
